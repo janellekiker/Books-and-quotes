005998fe2452f272fd38a6e7a64a9318b33c67a5
Janelle Kiker <janellekiker@gmail.com>
2023-03-14 16:53:03 -0500

    book model registered to admin

--- books/admin.py
+++ books/admin.py
@@ -1,3 +1,13 @@
 from django.contrib import admin
+from books.models import Book
 
-# Register your models here.
+
+@admin.register(Book)
+class BookAdmin(admin.ModelAdmin):
+    list_display = (
+        "title",
+        "picture",
+        "summary",
+        "started",
+        "finished",
+    )
