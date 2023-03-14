ee077ee86dc0db95154cfc2f427a740a24e51aa9
Janelle Kiker <janellekiker@gmail.com>
2023-03-14 16:49:38 -0500

    book model added

--- books/models.py
+++ books/models.py
@@ -1,6 +1,6 @@
 from django.db import models
 
-class Books(models.Model):
+class Book(models.Model):
     title = models.CharField(max_length=200)
     picture = models.URLField(max_length=800)
     summary = models.TextField()
